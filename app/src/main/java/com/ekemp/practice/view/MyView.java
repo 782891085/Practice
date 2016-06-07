0c9cfc1a9f2868004fba6c94fd8517442585f573
zjx <782891085@qq.com>
2016-06-07 10:26:15 +0800

    增加一个类

---
+++ app/src/main/java/com/ekemp/practice/view/MyView.java
@@ -0,0 +1,26 @@
+package com.ekemp.practice.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by zjx on 2016/6/7.
+ */
+public class MyView extends View
+{
+    public MyView(Context context)
+    {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+}
